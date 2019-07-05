c0a549ce1d69a8778d7117c943cd411c38a7fc9c
Simone Karin Lehmann <simone@lisanet.de>
2019-07-05 14:48:40 +0200

    move configurable variables DEFAULT_LOCATION and ENABLE_NOTIFICATIONS to config file. Generate a default config file on installation unless it doesn't exist. Quote DEFAULT_LOCATION, if configurable, it could contain spaces.

--- locationchanger.sh
+++ locationchanger.sh
@@ -13,7 +13,7 @@ cat << "EOT" | sudo tee ${SCRIPT_NAME} > /dev/null
 
 # This script changes network Location based on the name of Wi-Fi network.
 DEFAULT_LOCATION='Automatic'
-ENABLE_NOTIFICATIONS=1 # To enable notifications, set to 1. For verbose notifications, set to 2. To disable, set to 0.
+ENABLE_NOTIFICATIONS=1
 CONFIG_FILE="$HOME/Library/Application Support/LocationChanger/LocationChanger.conf"
 SCRIPT_DIR="$HOME/Library/Application Support/LocationChanger" # directory for scripts attached to Locations
 LOGFILE=${HOME}/Library/Logs/LocationChanger.log
@@ -49,6 +49,18 @@ fi
 
 ts "Connected to '${SSID}'"
 
+# read some default variables from config file, if they exist
+if [ -e "${CONFIG_FILE}" ]; then
+    VALUE=$(parse_config General | grep ENABLE_NOTIFICATIONS= | cut -d = -f 2)
+    if [ "$VALUE" != "" ]; then
+        ENABLE_NOTIFICATIONS=$VALUE
+    fi
+    VALUE=$(parse_config General | grep DEFAULT_LOCATION= | cut -d = -f 2)
+    if [ "$VALUE" != "" ]; then
+        DEFAULT_LOCATION="$VALUE"
+    fi
+fi
+
 # escape the SSID string for better string handling in our logic below
 ESSID=$(echo "${SSID}" | sed 's/[.[\*^$]/\\\\&/g')
 
@@ -81,7 +93,7 @@ if [ -z "${NEW_LOCATION}"] && echo "${LOCATION_NAMES}" | grep -q "^${ESSID}$"; t
     ts "Location '${SSID}' was found and matches the SSID. Will switch the Location to '${NEW_LOCATION}'"
 # if still not found, try to use the DEFAULT_LOCATION
 elif [ -z "${NEW_LOCATION}"] && echo "${LOCATION_NAMES}" | grep -q "^${DEFAULT_LOCATION}$"; then
-    NEW_LOCATION=${DEFAULT_LOCATION}
+    NEW_LOCATION="${DEFAULT_LOCATION}"
     NOTIFICATION_STRING="Changing from '${CURRENT_LOCATION}' to default Location '${DEFAULT_LOCATION}'"
     ts "Location '${SSID}' was not found. Will default to '${DEFAULT_LOCATION}'"
 # if we arrived here, something went awry
@@ -124,6 +136,31 @@ exit 0
 EOT
 
 sudo chmod +x ${SCRIPT_NAME}
+
+# generate a default config file if it doesn't exists
+APP_SUPPORT_DIR="$HOME/Library/Application Support/LocationChanger"
+if [ ! -e "${APP_SUPPORT_DIR}/LocationChanger.conf" ]; then
+mkdir -p "${APP_SUPPORT_DIR}"
+cat > "$APP_SUPPORT_DIR/LocationChanger.conf" << EOT
+[General]
+# specify the default Location to use. The default is 'Automatic'
+#DEFAULT_LOCATION=Automatic
+# To enable notifications, set to 1. For verbose notifications, set to 2. To disable, set to 0.
+#ENABLE_NOTIFICATIONS=1
+
+[Automatic]
+# [Automatic] defines a mapping for Wi-Fi Network SSIDs to Location names as key-value pairs.
+# Spaces are supported for both the SSID as well as the Location name, but all spaces
+# around the '=' will be trimmed. Additionally, do not enclose the SSID or Location in quotes
+# SSID=Location name
+
+[Manual]
+# This section contains a list of Location names for which autodetection and Location
+# switching should be ignored.
+# Wi-Fi Only
+
+EOT
+fi
 
 mkdir -p ${LAUNCH_AGENTS_DIR}
 cat > ${PLIST_NAME} << EOT
